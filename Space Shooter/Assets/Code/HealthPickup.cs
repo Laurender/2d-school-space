72a62ede4f5443ef03664ed453ff15ce0ec045a6
Lauri Johannes Leponiemi <lauri.leponiemi@cs.tamk.fi>
2017-11-09 23:55:20 +0200

    Started Assignment 3

---
+++ Space Shooter/Assets/Code/HealthPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class HealthPickup : MonoBehaviour {
+
+		[SerializeField]
+		private int _healthHealed = 20;
+		[SerializeField]
+		private float _lifetime = 5.0f;
+		[SerializeField]
+		private float _speed;
+
+		private Rigidbody2D _rigidbody;
+		private Vector2 _direction;
+
+		protected virtual void Awake () {
+			
+			_rigidbody = GetComponent<Rigidbody2D>();
+
+			if (_rigidbody == null)
+			{
+				Debug.LogError("No Rigidbody2D component was found from the GameObject");
+			}
+		}
+		
+		// Update is called once per frame
+		protected void Update () {
+			
+		}
+	}
+}
